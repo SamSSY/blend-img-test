1be73240ca2c5cd003e641a4482f699f21fe8cf2
SamSSY <sam7822567@gmail.com>
2016-02-02 07:15:15 +0800

    0202/0717

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { render } from 'react-dom';
 import Dropzone from 'react-dropzone';
+import './main.scss';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 injectTapEventPlugin();
 
 const initialState = {
-
+  files: []
 };
 
 class MainBody extends React.Component {
@@ -15,15 +16,43 @@ class MainBody extends React.Component {
     this.state = initialState;
   }
 
+  componentDidMount() {
+  }
+
+  componentDidUpdate() {
+    $('img').on('load', () => {
+      console.log('img loaded.');
+    });
+  }
+
   onDrop(files) {
-    console.log('Received files: ', files);
+    this.setState({
+      files: files
+    });
   }
 
   render() {
+    const { files } = this.state;
+    const imgStyles = {
+      height: '300px'
+    };
+    const dropzoneStyles = {
+      height: '60%',
+      width: '45%',
+      border: '1px dashed'
+    };
+    console.log('render!', files);
+    console.log('length', files.length);
+
     return (
-      <div>
-        <Dropzone onDrop={this.onDrop}>
-          <div>Try dropping some files here, or click to select files to upload.</div>
+      <div style={{ height: '100%' }} >
+        <Dropzone multiple={false} style={dropzoneStyles} onDrop={this.onDrop.bind(this)}>
+          {files.length > 0 ?
+            <div>
+              <img id='originalImg' key={files[0].size}
+                    src={files[0].preview} style={imgStyles} />
+            </div> : <div>Try dropping some files here, or click to select files to upload.</div>
+          }
         </Dropzone>
       </div>
     );
