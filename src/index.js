a30359745774f6fe5016c570114ae20c41aabfbb
SamSSY <sam7822567@gmail.com>
2016-02-02 07:56:51 +0800

    0202/0759

--- src/index.js
+++ src/index.js
@@ -21,7 +21,20 @@ class MainBody extends React.Component {
 
   componentDidUpdate() {
     $('img').on('load', () => {
-      console.log('img loaded.');
+      console.log('img uploaded.');
+      const imgWH = $('#originalImg').css(['width', 'height']);
+      console.log(imgWH);
+      console.log(imgWH.width);
+      console.log(imgWH.height);
+      let w = parseInt(imgWH.width);
+      let h = parseInt(imgWH.height);
+      if (w > h && w > 350) {
+        console.log('width > 350px');
+        $('#originalImg').css({ 'width':  '350px' });
+      } else if (w < h && h > 350) {
+        console.log('height > 350px');
+        $('#originalImg').css({ 'height':  '350px' });
+      }
     });
   }
 
@@ -34,12 +47,13 @@ class MainBody extends React.Component {
   render() {
     const { files } = this.state;
     const imgStyles = {
-      height: '300px'
+      //height: '300px',
     };
     const dropzoneStyles = {
-      height: '60%',
-      width: '45%',
-      border: '1px dashed'
+      height: '350px',
+      width: '350px',
+      border: '1px dashed',
+      margin: '20px'
     };
     console.log('render!', files);
     console.log('length', files.length);
@@ -48,7 +62,7 @@ class MainBody extends React.Component {
       <div style={{ height: '100%' }} >
         <Dropzone multiple={false} style={dropzoneStyles} onDrop={this.onDrop.bind(this)}>
           {files.length > 0 ?
-            <div>
+            <div style={{ overflowX: 'hidden', overflowY: 'hidden' }} >
               <img id='originalImg' key={files[0].size}
                     src={files[0].preview} style={imgStyles} />
             </div> : <div>Try dropping some files here, or click to select files to upload.</div>
